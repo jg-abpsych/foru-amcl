f801f5f5f4b765575faa745eaeab2e16bf89d98d
jg-abpsych <guniojulius@gmail.com>
2026-04-20 15:58:28 +0800

    Update gamescript.js

--- gamescript.js
+++ gamescript.js
@@ -73,7 +73,7 @@ function handleYesClick() {
         showTeaseMessage(msg)
         return
     }
-    window.location.href = 'yes.html'
+    window.location.href = 'gameyes.html'
 }
 
 function showTeaseMessage(msg) {
